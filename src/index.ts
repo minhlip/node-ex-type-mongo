e8797bdbe9c2ff250b121a60e4c0a1324b14cd17
minhlip <minhlips2002@gmail.com>
2024-05-06 22:04:50 +0700

    mongodb setup, env setup, mongoose initialize, user schema initialize, user action initialize

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,36 @@
-import express from "express"
-import http from "http"
-import bodyParser from "body-parser"
-import cookieParser from "cookie-parser"
-import compression from "compression"
-import cors from "cors"
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
-const app = express()
+const app = express();
 
-const PORT = 8080
+const PORT = 8080;
 
-app.use(cors({
-  credentials: true
-}))
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
-app.use(compression())
-app.use(cookieParser())
-app.use(bodyParser.json())
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
 
-
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
-})
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+mongoose.Promise = Promise;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error: Error) => console.log('Error: ', error));
